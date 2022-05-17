a9cacd694e82bbab9274607967eae291064787a0
Stephen Moore <judo2000@comcast.net>
2022-05-17 13:57:57 -0600

    Create tickets page to view all a user's tickets

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "/api/tickets/";
 
-// Create new tickets
+// Create new ticket
 const createTicket = async (ticketData, token) => {
   const config = {
     headers: {
@@ -15,8 +15,22 @@ const createTicket = async (ticketData, token) => {
   return response.data;
 };
 
+// Get user tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
 const ticketService = {
   createTicket,
+  getTickets,
 };
 
 export default ticketService;
